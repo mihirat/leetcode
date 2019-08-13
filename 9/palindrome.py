0dc78291903e5fb8685aa92d08fc05017ae2cb5a
mihirat <teketen5656@gmail.com>
2019-08-13 17:55:57 +0900

    9: palindrome (#2)
    * palindrome
    
    * palindrome

---
+++ 9/palindrome.py
@@ -0,0 +1,10 @@
+import copy
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        arr = [s for s in str(x)]
+        arr_rev = copy.copy(arr)
+        arr_rev.reverse()
+        if arr == arr_rev:
+            return True
+        return False
